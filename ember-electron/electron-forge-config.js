f3c557a02300cd904356a41c4f2b49f149cb31a0
jamessalvatore <jsalvatore171@gmail.com>
2018-07-23 16:47:02 -0400

    Added name to forge config so mac executable has correct name.

--- ember-electron/electron-forge-config.js
+++ ember-electron/electron-forge-config.js
@@ -1,33 +1,27 @@
-  module.exports = {
-  "make_targets": {
-    "win32": [
-      "squirrel"
-    ],
-    "darwin": [
-      "zip"
-    ],
-    "linux": [
-      "deb",
-      "rpm"
-    ]
+module.exports = {
+  make_targets: {
+    win32: ['squirrel'],
+    darwin: ['zip'],
+    linux: ['deb', 'rpm']
   },
-  "electronPackagerConfig": {
-    "platform": "all",
-    "packageManager": "npm",
-    "executableName": "Aperture",
-    "icon": __dirname + "/resources/aperture"
+  electronPackagerConfig: {
+    platform: 'all',
+    packageManager: 'npm',
+    executableName: 'Aperture',
+    name: 'Aperture',
+    icon: __dirname + '/resources/aperture'
   },
-  "electronWinstallerConfig": {
-    "name": "aperture_web"
+  electronWinstallerConfig: {
+    name: 'aperture_web'
   },
-  "electronInstallerDebian": {},
-  "electronInstallerRedhat": {},
-  "github_repository": {
-    "owner": "",
-    "name": ""
+  electronInstallerDebian: {},
+  electronInstallerRedhat: {},
+  github_repository: {
+    owner: '',
+    name: ''
   },
-  "windowsStoreConfig": {
-    "packageName": "",
-    "name": "apertureapp"
+  windowsStoreConfig: {
+    packageName: '',
+    name: 'apertureapp'
   }
 };

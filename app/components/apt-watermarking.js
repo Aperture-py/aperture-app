104f4a07a6db5a87aeee2de3032b23adc9222e01
jamessalvatore <jsalvatore171@gmail.com>
2018-08-05 17:35:19 -0400

    Fixed watermark image not showing after upload.

--- app/components/apt-watermarking.js
+++ app/components/apt-watermarking.js
@@ -4,9 +4,24 @@ export default Component.extend({
   us: service('upload'),
   fq: service('file-queue'),
   image: null,
+  init() {
+    this._super(...arguments);
+    const watermark = this.get('fq')
+      .find('watermarks')
+      .get('files.firstObject');
+    if (watermark) {
+      this.set('image', watermark);
+    }
+  },
   actions: {
     watermarkAdded(file) {
-      if (this.get('image') === null) {
+      console.log('watermark image added: ', file);
+
+      if (
+        this.get('fq')
+          .find('watermarks')
+          .get('files.length') == 1
+      ) {
         this.set('image', file);
       } else {
         // Only supporting 1 watermark image for now.
@@ -23,5 +38,10 @@ export default Component.extend({
     this.get('fq')
       .find('watermarks')
       .remove(image);
+
+    console.log(
+      'watermark queue after remove: ',
+      this.get('fq').find('watermarks')
+    );
   }
 });
